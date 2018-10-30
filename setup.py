65b8b859d2caef03c4225577959491f6545299e6
JC Connell <jamescarltonconnell@gmail.com>
2018-10-30 15:58:30 -0400

    Update requests dependency to ~> 2.20.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='magicseaweed',
-    version='1.0.1',
+    version='1.0.2',
     description='Provides API wrapper to magicseaweed.com.',
     url='https://github.com/jcconnell/python-magicseaweed',
     license='MIT',
     author='jcconnell',
     author_email='jamescarltonconnell@gmail.com',
     packages=find_packages(),
-    install_requires=['requests ~> 2.20.0'],
+    install_requires=['requests>=2.20.0'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
